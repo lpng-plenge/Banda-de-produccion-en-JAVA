f6f92cfcfd88ad894982c5d5a72443e2e83d363a
luis pablo nieto gil <56002574+lpng-plenge@users.noreply.github.com>
2021-05-04 18:27:06 -0500

    Actualizacion BD

--- dbbandaproduccion.sql
+++ dbbandaproduccion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2021 a las 03:54:06
+-- Tiempo de generación: 05-05-2021 a las 00:55:00
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.3
 
