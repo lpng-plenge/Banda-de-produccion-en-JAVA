8f5d57d0ea7191ddd3efe51049d2a1b0106fe8f7
luis pablo nieto gil <56002574+lpng-plenge@users.noreply.github.com>
2021-06-08 11:15:30 -0500

    Sql Actualizado

--- dbbandaproduccion.sql
+++ dbbandaproduccion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: sql3.freemysqlhosting.net
--- Tiempo de generación: 28-05-2021 a las 22:05:08
+-- Tiempo de generación: 02-06-2021 a las 04:57:39
 -- Versión del servidor: 5.5.54-0ubuntu0.12.04.1
 -- Versión de PHP: 7.0.33-0ubuntu0.16.04.16
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `sql3415613`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producciones`
+--
+
+CREATE TABLE `producciones` (
+  `usuario_id` varchar(30) NOT NULL,
+  `entrada` int(11) NOT NULL DEFAULT '0',
+  `salida` int(11) NOT NULL DEFAULT '0',
+  `defectuoso` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producciones`
+--
+
+INSERT INTO `producciones` (`usuario_id`, `entrada`, `salida`, `defectuoso`) VALUES
+('jorge', 4, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -46,21 +66,38 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `aPaterno`, `aMaterno`, `telefono`, `direccion`, `usuario`, `password`, `tipo`, `Estatus`) VALUES
-(1, 'Luis Pablo', 'Nieto', 'Gil', '3313507243', 'Paseo del Dromedario 99, Colonia CD Bugambilias, CP 45238, Zapopan, Jalisco, Mexico', 'lpngAdmin', '123', 1, 0),
-(2, 'Jorge', 'Urbina', 'Tapia', '3313507243', 'Guadalajara', 'jorge', '123', 0, 0),
-(3, 'Sebastian', 'Alegria', 'Uribe', '3931173449', 'La Barca', 'sebA', '123', 1, 0),
 (4, 'Carlos Daniel', 'Camcho', 'Chavez', '3330672020', 'Zapopan', 'danCa', '123', 1, 0),
-(6, 'root', 'na', 'na', 'na', 'na', 'root', 'root', 1, 0);
+(2, 'Jorge', 'Urbina', 'Tapia', '3313507243', 'Guadalajara', 'jorge', '123', 0, 0),
+(1, 'Luis Pablo', 'Nieto', 'Gil', '3313507243', 'na', 'lpng', '123', 1, 0),
+(5, 'asd', 'asd', 'asd', 'asd', 'asd', 'root', 'root', 1, 0),
+(3, 'Sebastian', 'Alegria', 'Uribe', '3931173449', 'La Barca', 'sebA', '123', 1, 0);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `producciones`
+--
+ALTER TABLE `producciones`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`usuario`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producciones`
+--
+ALTER TABLE `producciones`
+  ADD CONSTRAINT `producciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
